fdc9680ff91ade1413d73885b8d7008ae6a8ec8a
fallinnadim <nadim@mhs.mdp.ac.id>
2024-02-18 01:20:26 +0700

    finishing task

--- build_push_image.sh
+++ build_push_image.sh
@@ -3,4 +3,8 @@ docker build -t item-app:v1 .
 # Command untuk melist semua docker image yang berada pada lokal
 docker images
 # Command untuk merubah namespace dan tag image menjadi format yang ditentukan sesuai platform (Dockerhub / Github Package)
-docker tag item-app:v1 fallinnadim/item-app:v1
+docker tag item-app:v1 ghcr.io/fallinnadim/item-app:v1
+# Command untuk login ke github container registry, disini digunakan ENV Variable dengan nama GITHUB_PAT (Personal Access Token)
+echo $GITHUB_PAT | docker login ghcr.io -u fallinnadim --password-stdin
+# Command untuk push image yang sudah dibuat ke github container registry
+docker push ghcr.io/fallinnadim/item-app:v1
